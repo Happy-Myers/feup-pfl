eae0e6416fce28e1050f66dff899cad293a03b97
Gonçalo Marques <up202006874@edu.fe.up.pt>
2022-09-23 11:53:51 +0100

    some more exercises from class 1

--- aula1/ex1.hs
+++ aula1/ex1.hs
@@ -44,7 +44,62 @@ binom n k = div (product [1 .. n]) (product [1 .. k] * product [1 .. n-k])
 
 --b)
 binom' :: Integer -> Integer -> Integer
-binom n k 
-| k < n-k = div (product [n-k+1 .. n]) (product [1 .. k])
-| k >= n-k = div (product [k+1 .. n]) (product [1 .. n-k])
---error here fix later
+binom' n k 
+    | k < n-k = div (product [n-k+1 .. n]) (product [1 .. k])
+    | k >= n-k = div (product [k+1 .. n]) (product [1 .. n-k])
+
+--ex 1.6
+raizes :: Float -> Float -> Float -> (Float, Float)
+raizes a b c = ((-b + sqrt d)/(2 * a), (-b - sqrt d)/(2 * a))
+    where d = b**2 - 4 * a * c
+
+--ex 1.8
+
+--a)
+segundo xs = head(tail xs)
+
+--b) 
+trocar (x, y) = (y,x)
+
+--c)
+par x y = (x,y)
+
+--d)
+dobro x = 2 * x
+
+--e)
+metade x = x/2
+
+--f)
+minuscula x = x >= 'a' && x <= 'z'
+
+--g)
+intervalo x a b = x >= a && x <= b
+
+--h)
+palindromo xs = reverse xs == xs
+
+--i)
+twice f x = f (f x)
+
+--ex 1.9
+classifica :: Int -> String 
+classifica x = if x <= 9 then "reprovado" else (if x <= 12 then "suficiente" else (if x <= 15 then "bom" else (if x <= 18 then "muito bom" else "muito bom com distincao")))
+
+classifica' :: Int -> String
+classifica' x 
+    | x <= 9 = "reprovado" 
+    | x <= 12 = "suficiente"
+    | x <= 15 = "bom"
+    | x <= 18 = "muito bom"
+    | x <= 20 = "muito bom com distincao"
+    | otherwise = "valor invalido"
+
+--ex 1.10
+classificaIMC :: Float -> Float -> String
+classificaIMC p a 
+    | IMC < 18.5 = "baixo peso" 
+    | IMC < 25 = "peso normal"
+    | IMC < 30 = "excesso de peso"
+    | otherwise = "obesidade"
+    where IMC = p / a**2
